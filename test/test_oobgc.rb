228f34715f7117c4c1f6e6662c64f8eabc9fc8a3
Alex Snaps <alex.snaps@shopify.com>
2019-03-28 15:35:55 -0400

    Some, somewhat flaky, tests

--- test/test_oobgc.rb
+++ test/test_oobgc.rb
@@ -26,52 +26,61 @@ class TestOOBGC < Minitest::Test
   #   GC::OOB.clear
   # end
 
-  # def test_oob_sweep
-  #   GC.start(immediate_sweep: false)
-  #   assert_equal false, GC.latest_gc_info(:immediate_sweep)
+  def test_oob_sweep
+    assert_equal true, GC::OOB.setup
+    GC::OOB.clear
+    GC.start
+    150_000.times{ Object.new }
+    GC.start(immediate_sweep: false)
+    assert_equal false, GC.latest_gc_info(:immediate_sweep)
+    150_000.times{ Object.new }
+    before = GC::OOB.stat(:sweep_count)
+    assert_equal true, GC::OOB.run
+    info = GC.latest_gc_info
+    assert_operator GC::OOB.stat(:sweep_count), :>, before
+  ensure
+    assert_equal true, GC::OOB.teardown
+  end
 
-  #   before = GC::OOB.stat(:sweep_count)
-  #   while !GC::OOB.run do
-  #     GC.start(immediate_sweep: false)
-  #     assert_equal false, GC.latest_gc_info(:immediate_sweep)
-  #     (rand(250_000)).times{ Object.new }
-  #   end
-  #   assert_operator GC::OOB.stat(:sweep_count), :>, before
-  # end
+  def test_oob_mark
+    assert_equal true, GC::OOB.setup
+    GC::OOB.clear
+    oob = 0
+    before = GC.count
+    minor = GC.stat(:minor_gc_count)
 
-  # def test_oob_mark
-  #   oob = 0
-  #   before = GC.count
-  #   minor, major = GC.stat(:minor_gc_count), GC.stat(:major_gc_count)
+    20.times do
+      5_000.times{ Object.new }
+      oob += 1 if GC::OOB.run
+    end
 
-  #   20.times do
-  #     (10_000 + rand(25_000)).times{ Object.new }
-  #     oob += 1 if GC::OOB.run
-  #   end
+    assert_operator oob, :>=, GC::OOB.stat(:minor_count)
+    assert_operator GC.stat(:minor_gc_count) - minor - oob + GC::OOB.stat(:major_count), :>=, 0
+  ensure
+    assert_equal true, GC::OOB.teardown
+  end
 
-  #   assert_equal 0, GC.count - before - oob
-  #   assert_equal 0, GC.stat(:minor_gc_count) - minor - oob
-  #   assert_equal major, GC.stat(:major_gc_count)
-  # end
+  def test_oob_major_mark
+    assert_equal true, GC::OOB.setup
+    GC::OOB.clear
+    oob = 0
+    before = GC.count
+    major = GC.stat(:major_gc_count)
+    list = []
 
-  # def test_oob_major_mark
-  #   oob = 0
-  #   before = GC.count
-  #   minor, major = GC.stat(:minor_gc_count), GC.stat(:major_gc_count)
-  #   list = []
+    20.times do
+      2_000.times do
+        list << JSON.parse('{"hello":"world", "foo":["bar","bar"], "zap":{"zap":"zap"}}')
+        list.shift if list.size > 2_000
+      end
+      oob += 1 if GC::OOB.run
+    end
 
-  #   20.times do
-  #     2_000.times do
-  #       list << JSON.parse('{"hello":"world", "foo":["bar","bar"], "zap":{"zap":"zap"}}')
-  #       list.shift if list.size > 2_000
-  #     end
-  #     oob += 1 if GC::OOB.run
-  #   end
-
-  #   assert_equal oob, GC::OOB.stat(:count)
-  #   minor = GC.stat(:minor_gc_count) - minor
-  #   major = GC.stat(:major_gc_count) - major
-  #   assert_equal 1, GC.count - before - major - minor - GC::OOB.stat(:sweep_count)
-  #   assert_equal 1, GC.count - before - GC::OOB.stat(:major_count) - GC::OOB.stat(:minor_count) - GC::OOB.stat(:sweep_count)
-  # end
+    assert_equal oob, GC::OOB.stat(:count)
+    major = GC.stat(:major_gc_count) - major
+    assert_operator major, :>, 0
+    assert_operator GC::OOB.stat(:major_count), :>, 0
+  ensure
+    assert_equal true, GC::OOB.teardown
+  end
 end
